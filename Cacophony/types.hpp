6823c7256d6e7d30932d3bb981145ac5976fb41a
Coranna Howard <me@komiga.com>
2014-03-30 20:17:21 -0400

    types: added enable_ser_of.

--- Cacophony/types.hpp
+++ Cacophony/types.hpp
@@ -14,6 +14,8 @@ see @ref index or the accompanying LICENSE file for full text.
 
 #include <type_traits>
 
+#include <duct/utility.hpp>
+
 namespace Cacophony {
 
 // Forward declarations
@@ -54,26 +56,41 @@ enum class tag_write : unsigned {};
 /**
 	SFINAE.
 
-	@tparam value Boolean to enable or disable.
+	@tparam C Constraints.
 */
 template<
-	bool const value,
+	bool const C,
 	class T = void
 >
 using enable = typename std::enable_if<
-	value,
+	C,
 	T
 >::type;
 
 /**
 	SFINAE with type @c ser_result_type.
 
-	@tparam value Boolean to enable or disable.
+	@tparam C Constraints.
 */
 template<
-	bool const value
+	bool const C
 >
-using enable_ser = enable<value, ser_result_type>;
+using enable_ser = enable<C, ser_result_type>;
+
+/**
+	SFINAE with type @c ser_result_type for specific serializers.
+
+	@tparam Ser Serializer type.
+	@tparam ...M Serializers to match.
+*/
+template<
+	class Ser,
+	class... M
+>
+using enable_ser_of = enable<
+	duct::is_same_any<Ser, M...>::value,
+	ser_result_type
+>;
 
 /** @} */ // end of doc-group types
 
