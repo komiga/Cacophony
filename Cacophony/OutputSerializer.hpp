67eba7afb5f460621c74a6f91f152f499e3ff4ac
Coranna Howard <me@komiga.com>
2015-01-07 20:19:57 -0500

    Updated copyrights and trimmed down file headers.

--- Cacophony/OutputSerializer.hpp
+++ Cacophony/OutputSerializer.hpp
@@ -1,10 +1,8 @@
 /**
+@copyright MIT license; see @ref index or the accompanying LICENSE file.
+
 @file
 @brief Output serializer.
-
-@author Timothy Howard
-@copyright 2014 Timothy Howard under the MIT license;
-see @ref index or the accompanying LICENSE file for full text.
 */
 
 #pragma once
